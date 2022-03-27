4fc113d24547ff0a870c8495e7957b7c29e3c973
weldnor <kolesov.anton.s@gmail.com>
2022-03-27 22:58:50 +0300

    Fixed ws

--- src/app/routed/main/pages/room/room.page.ts
+++ src/app/routed/main/pages/room/room.page.ts
@@ -1,31 +1,38 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {KurentoService} from '../../../../features/core/services/kurento.service';
-import {FormControl} from '@angular/forms';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RoomService} from "../../../../features/core/services/room.service";
+import {StreamService} from "../../../../features/core/services/stream.service";
+import {ActivatedRoute} from "@angular/router";
+import {WebsocketService} from "../../../../features/core/services/websocket.service";
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.page.html',
   styleUrls: ['./room.page.scss']
 })
-export class RoomPage implements AfterViewInit {
-  @ViewChild('root') rootElement!: ElementRef;
-  userIdForm = new FormControl('');
-  // private userId = 1;
-  private roomId = 1;
+export class RoomPage implements OnInit, OnDestroy {
+
+  roomId: string;
 
   constructor(
-    private readonly kurentoService: KurentoService
+    private readonly roomService: RoomService,
+    private readonly streamService: StreamService,
+    private readonly route: ActivatedRoute,
+    private readonly websocketService: WebsocketService,
   ) {
-
   }
 
+  async ngOnInit() {
+    this.roomId = this.route.snapshot.paramMap.get('id');
 
-  ngAfterViewInit(): void {
+    let client = await this.websocketService.getClient();
+    client.subscribe("/queue/test", message => {
+      console.log(message.body);
+    })
 
+    client.send("/app/test/echo", {}, "hello world!");
   }
 
-  onConnectButtonClick(): void {
-    const userId = +this.userIdForm.value;
-    this.kurentoService.start(userId, this.roomId, this.rootElement.nativeElement);
+  ngOnDestroy(): void {
+    // nothing there
   }
 }
