a1706d448dfaa84c48ca99f4963701331704b089
weldnor <kolesov.anton.s@gmail.com>
2022-05-14 00:35:06 +0300

    Fixed bug with login

--- src/app/routed/main/main-routing.module.ts
+++ src/app/routed/main/main-routing.module.ts
@@ -7,16 +7,17 @@ import {UserPage} from './pages/user/user.page';
 import {RoomPage} from './pages/room/room.page';
 import {SettingsPage} from './pages/settings/settings.page';
 import {JoinPage} from "./pages/join/join.page";
+import {AuthGuard} from "../../features/core/services/auth.guard";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'prefix'},
-  {path: 'home', component: HomePage},
+  {path: 'home', component: HomePage, canActivate: [AuthGuard]},
   {path: 'login', component: LoginPage},
   {path: 'register', component: RegisterPage},
   {path: 'users/:id', component: UserPage},
   {path: 'rooms/:id', component: RoomPage},
   {path: 'join/:id', component: JoinPage},
   {path: 'settings', component: SettingsPage},
+  {path: '**', redirectTo: '/home'},
 ];
 
 @NgModule({
